43ed94f64bf6ae47289524bf871775f903bf5699
PeterClaydon <peter.claydon@continuumbridge.com>
2014-03-13 19:23:57 +0000

    Change CVS date format

--- scripts/makecsv.py
+++ scripts/makecsv.py
@@ -15,7 +15,12 @@ from dropbox.datastore import DatastoreError, DatastoreManager, Date, Bytes
 from pprint import pprint
 import time
 import os, sys
-from utilities import niceTime
+
+def niceTime(timeStamp):
+    localtime = time.localtime(timeStamp)
+    milliseconds = '%03d' % int((timeStamp - int(timeStamp)) * 1000)
+    now = time.strftime('%Y-%m-%d %H:%M:%S', localtime)
+    return now
 
 class CheckEEW():
     def __init__(self, argv):
@@ -40,50 +45,7 @@ class CheckEEW():
         
         self.manager = DatastoreManager(self.client)
         self.process()
-    
-    def matrix_to_string(self,matrix, header=None):
-        """
-        Return a pretty, aligned string representation of a nxm matrix.
-    
-        This representation can be used to print any tabular data, such as
-        database results. It works by scanning the lengths of each element
-        in each column, and determining the format string dynamically.
-    
-        @param matrix: Matrix representation (list with n rows of m elements).
-        @param header: Optional tuple or list with header elements to be displayed.
-        """
-        if type(header) is list:
-            header = tuple(header)
-        lengths = []
-        if header:
-            for column in header:
-                lengths.append(len(column))
-        for row in matrix:
-            for column in row:
-                i = row.index(column)
-                column = str(column)
-                cl = len(column)
-                try:
-                    ml = lengths[i]
-                    if cl > ml:
-                        lengths[i] = cl
-                except IndexError:
-                    lengths.append(cl)
-    
-        lengths = tuple(lengths)
-        format_string = ""
-        for length in lengths:
-            format_string += "%-" + str(length) + "s "
-        format_string += "\n"
-    
-        matrix_str = ""
-        if header:
-            matrix_str += format_string % header
-        for row in matrix:
-            matrix_str += format_string % tuple(row)
-    
-        return matrix_str
-    
+   
     def process(self):
         for bridge in self.bridges:
             print bridge
