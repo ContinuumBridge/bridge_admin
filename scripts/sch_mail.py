171a59761dc6a16abe88a017a5c012881175729b
PeterClaydon <peter.claydon@continuumbridge.com>
2014-11-01 20:41:21 +0000

    Improved

--- scripts/sch_mail.py
+++ scripts/sch_mail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # sch_email.py
-# Copyright (C) ContinuumBridge Limited, 2013-14 - All Rights Reserved
+# Copyright (C) ContinuumBridge Limited, 2014 - All Rights Reserved
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
 # Written by Peter Claydon
@@ -12,11 +12,18 @@ import json
 import time
 import click
 import os, sys
+import re
 import smtplib
 from itertools import cycle
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.MIMEImage import MIMEImage
 
+#Constants
+tenMinutes         = 10 * 60
+oneHour            = 60 * 60
+oneDay             = oneHour * 24
+fileName           = "Sirona table template.htm"
 
 def nicetime(timeStamp):
     localtime = time.localtime(timeStamp)
@@ -33,94 +40,203 @@ def nicehours(timeStamp):
     now = time.strftime('%H:%M:%S', localtime)
     return now
 
+def stepHourMin(timeStamp):
+    localtime = time.localtime(timeStamp)
+    now = time.strftime('%H%M', localtime)
+    return now
+
 def epochtime(date_time):
     pattern = '%Y-%m-%d %H:%M:%S'
     epoch = int(time.mktime(time.strptime(date_time, pattern)))
     return epoch
 
+def activeInTenMinutes(series, time):
+    for s in series:
+        if s["t"] >= time and s["t"] < time + tenMinutes:
+            return True
+    return False
+
+def powerInTenMinutes(series, time):
+    for s in series:
+        if s["t"] >= time and s["t"] < time + tenMinutes:
+            if s["v"] > 10:
+                return True
+    return False
+
+def tempInTenMinutes(series, time):
+    for s in series:
+        if s["t"] >= time and s["t"] < time + tenMinutes:
+            return "%2.1f" %s["v"]
+    return ""
+
+def readHTML(fileName):
+    with open(fileName, "r") as f:
+        h1 = f.read()
+    # Because there can be "illigal" ASCII characters in the HTML file:
+    i = 0
+    for c in h1:
+        if ord(c) > 127:
+            #print "Replaced:", c, ord(c)
+            h2 += "-"
+        else:
+            h2 += c
+    working = "h2"
+
+def replace(holder, value):
+    global h1, h2, working
+    if working == "h1":
+        h2 = h1.replace(holder, value)
+        working = "h2"
+    else:
+        h1 = h2.replace(holder, value)
+        working = "h1"
+
 @click.command()
 @click.option('--user', nargs=1, help='User name of email account.')
 @click.option('--password', prompt='Password', help='Password of email account.')
 @click.option('--start', nargs=1, help='Start time for getting time series in the format: 18-10-2014 11:05:02.')
-@click.option('--end', nargs=1, help='End time for getting time series in the format: 18-10-2014 11:05:02.')
 @click.option('--bid', nargs=1, help='The bridge ID to list.')
 @click.option('--to', nargs=1, help='The address to send the email to.')
 @click.option('--key', prompt='Geras API key', help='Your Geras API key. See http://geras.1248.io/user/apidoc.')
 
-def shc_email(user, password, start, end, bid, to, key):
+def shc_email(user, password, start, bid, to, key):
+    h1 = ""
+    h2 = ""
+    working = ""
     me = "Sirona Care and Health <sch@continuumbridge.com>"
     if not bid:
         print "You must provide a bridge ID using the --bid option."
         exit()
     else:
+        print "Requesting list"
         r = requests.get('http://geras.1248.io/serieslist', auth=(key,''))
         allseries = json.loads(r.content)
         serieslist = []
         for t in allseries:
             if bid in t:
                 print t
-                if "binary" in t.lower():
-                    serieslist.append(t)
-        print "Processing:", serieslist
+                serieslist.append(t)
     if start:
         startTime = epochtime(start)
-        if end:
-            endTime = epochtime(end)
-        else:
-            endTime = time.time()
-        timed = True
-    elif end:
-        print "If you specify an end time, you must also specify a start time"
-        exit()
+        endTime = startTime + 60 * 60 * 24
     else:
-        timed = False
+        print "You must give a start time with --start"
+        exit()
+    # Read HTML file
+    with open(fileName, "r") as f:
+        h1 = f.read()
+    # Because there can be "illigal" ASCII characters in the HTML file:
+    i = 0
+    for c in h1:
+        if ord(c) > 127:
+            #print "Replaced:", c, ord(c)
+            h2 += "-"
+        else:
+            h2 += c
+    # Headers
+    h1 = h2.replace("nnn", bid)
+    h2 = h1.replace("&lt;date1&gt;", nicedate(startTime))
+    h1 = h2.replace("&lt;date2&gt;", nicedate(endTime))
+    working = "h1"
 
     timeseries = {}
-    html = "<span style=i\"font-family:Cursive;font-size:12px;font-style:normal;font-weight:normal;"
-    html += "text-decoration:none;text-transform:none;color:000000;\">"
-    html += "Here is the activity for the 24 hours ending 23:59 on " + nicedate(endTime)
-    html += " for bridge " + bid + ".</span>"
-    html += """ \
-        <style type="text/css">
-        .tg  {border-collapse:collapse;border-spacing:0;border-color:#aabcfe;}
-        .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#669;background-color:#e8edff;}
-        .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#039;background-color:#b9c9fe;}
-        .tg .tg-s6z2{text-align:center}
-        .tg .tg-214n{font-size:11px;text-align:center}
-        .tg .tg-5hgy{background-color:#D2E4FC;text-align:center}
-        </style>
-        """
+    col = 1
     for s in serieslist:
-        if timed:
-            #url = gerasurl + 'series/' + s +'?start=' + str(startTime) + '&end=' + str(endTime) +'&rollup=avg&interval=1h'
+        if not("battery" in s or "connected" in s or "luminance" in s or ("binary" and "kettle" in s)):
             url = gerasurl + 'series/' + s +'?start=' + str(startTime) + '&end=' + str(endTime)
-        else:
-            url = gerasurl + 'series' + s
-        print "url:", url
-        r = requests.get(url, auth=(key,''))
-        timeseries[s] = json.loads(r.content)
-
-        html += "<table class=\"tg\"><tr>"
-        html += "<th class=\"tg-s6z2\" colspan=\"2\">Activity for " + s + "</th>"
-        html +=  "<th class=\"tg-s6z2\"></th></tr>"
-        html += "<td class=\"tg-s6z2\">Time</td>"
-        html += "<td class=\"tg-s6z2\">" + "Sensor" + "</td></tr>"
-
-        t = timeseries[s]["e"]
-        previous_v = 0
-        for s in t:
-            if s["v"] == 1 and previous_v == 0:
-                action = "activated"
-                previous_v = 1
+            print "url:", url
+            r = requests.get(url, auth=(key,''))
+            timeseries[s] = json.loads(r.content)
+            series = timeseries[s]["e"]
+            ss = s.split('/')
+            for value in (ss[1], ss[2], ss[3]):
+                holder = "S_" + str(col) + "_name" + str(ss.index(value))
+                print "holder:", holder, " value:", value
+                if working == "h1":
+                    h2 = h1.replace(holder, value)
+                    working = "h2"
+                else:
+                    h1 = h2.replace(holder, value)
+                    working = "h1"
+            if series and ("door" in s.lower() or "cupboard" in s.lower()) and "binary" in s.lower():
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    #print "stepTime:", nicetime(stepTime), " holder:", holder
+                    if activeInTenMinutes(series, stepTime):
+                        value = "Open"
+                    else:
+                        value = ""
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
+            elif series and "power" in s.lower():
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    if powerInTenMinutes(series, stepTime):
+                        value = "On"
+                    else:
+                        value = ""
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
+            elif series and "temperature" in s.lower():
+                prev_temperature = "none"
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    value = tempInTenMinutes(series, stepTime)
+                    if value == "":
+                        if prev_temperature != "none":
+                            value = prev_temperature
+                    else:
+                        prev_temperature = value
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
+            elif series and "pir" in s.lower() and "binary" in s.lower():
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    if activeInTenMinutes(series, stepTime):
+                        value = "A"
+                    else:
+                        value = ""
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
             else:
-                previous_v = 0
-                action = "deactivated"
-            if action == "activated":
-                html += "<tr><td class=\"tg-s6z2\">"+nicehours(s["t"]) + "</td>" 
-                html += "<td class=\"tg-s6z2\">" + action + "</td></tr>"
-                #html += "<tr>\n" + "<td>" + nicetime(s["t"]) + "</td>\n" + "<td>" + action + "</td>" + "</tr>\n"
-        html += "</table>\n"
-    
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    value = "-"
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
+            col += 1
+
+    # Remove any unused holders
+    if working == "h1":
+        h2 = re.sub("S_[0-9]+_name[0-9]+", "", h1, 0)
+        h1 = re.sub("S_[0-9]+_[0-9]+", "", h2, 0)
+    else:
+        h1 = re.sub("S_[0-9]+_name[0-9]+", "", h2, 0)
+        h2 = re.sub("S_[0-9]+_[0-9]+", "", h1, 0)
+    if working == "h1":
+        htmlText = h1
+    else:
+        htmlText = h2
     # Create message container - the correct MIME type is multipart/alternative.
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Activity for bridge " + bid
@@ -131,8 +247,15 @@ def shc_email(user, password, start, end, bid, to, key):
     text = "Content only available with HTML email clients\n"
     # Record the MIME types of both parts - text/plain and text/html.
     part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
+    part2 = MIMEText(htmlText, 'html')
     
+    fp = open('image002.gif', 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+    # Define the image's ID as referenced above
+    msgImage.add_header('Content-ID', '<image002.gif>')
+    msg.attach(msgImage)
+
     msg.attach(part1)
     msg.attach(part2)
     mail = smtplib.SMTP('smtp.gmail.com', 587)
