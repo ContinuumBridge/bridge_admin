c0e6e5d048a21a16f4ce55d81c3b7711c42ac257
PeterClaydon <peter.claydon@continuumbridge.com>
2014-11-02 20:14:05 +0000

    Minor changes to sch_email files

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-/home/bridge/bridge_admin/emailing/sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID36 --to peter.claydon@continuumbridge.com
+./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID36 --to peter.claydon@continuumbridge.com
