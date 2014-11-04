9796fa65b898c13db9e6385fc559ce1a9b0a1555
PeterClaydon <peter.claydon@continuumbridge.com>
2014-11-04 15:15:31 +0000

    Updated

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #
+cd /home/bridge/bridge_admin/emailing
+date
 ./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID36 --to peter.claydon@continuumbridge.com
+date
+./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID12 --to peter.claydon@continuumbridge.com
