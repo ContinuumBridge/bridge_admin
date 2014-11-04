6fd5ba2b0a23e888b522baba5e6a443256b36eca
PeterClaydon <peter.claydon@continuumbridge.com>
2014-11-04 18:35:39 +0000

    Added crontab comment

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
+# Used with the following line in /etc/crontab:
+# 15 12   * * *   bridge  /home/bridge/bridge_admin/emailing/sch_list.sh >> /home/bridge/bridge_admin/emailing/sch_mail.log 2>&1
+#
 cd /home/bridge/bridge_admin/emailing
 date
 ./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID36 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
