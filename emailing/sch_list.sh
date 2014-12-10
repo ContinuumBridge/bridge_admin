d8c5c75e6840caf8ca2c4be3962421067242d1ff
martinxs <martin.sotheran@continuumbridge.com>
2014-12-10 13:24:57 +0000

    moved BIDs 12, 6 & 7 onto CBr_mail

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -11,13 +11,13 @@ date
 ./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID45 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
 
 # CBr
-date
-./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID12 --to "carson.bradbury@continuumbridge.com, martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
-date
-./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID6 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
-date
-./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID7 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
-
-# Eventually migrate all CBr to here
 cd /home/bridge/bridge_admin/emailing/CBr_mailing
+date
 ./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID11 --to martin.sotheran@continuumbridge.com
+date
+./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID12 --to "carson.bradbury@continuumbridge.com, martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+date
+./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID6 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+date
+./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID7 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+
