6bc6782aeb3ea19f52cf1d50d0e0fb1fc3e19808
martinxs <martin.sotheran@continuumbridge.com>
2015-02-19 13:44:07 +0000

    Added cbr-45 sch3 (now at MM) to mailer. Updated geras backup to be more robust

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -5,16 +5,14 @@
 #
 cd /home/bridge/bridge_admin/emailing
 # Sirona
-#date
-# waste of space for now
-#./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID44 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
+date
+./CBr_mailBW.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID45 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
 date
 ./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID36 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com, martyn.price@sirona-cic.org.uk"
-
 # CBr
 cd /home/bridge/bridge_admin/emailing/CBr_mailing
 date
-./CBr_mailBW.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID59 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
+./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID59 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
 #, richard.tarring@sirona-cic.org.uk" just whilst his bridge is dead
 date
 ./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID6 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"date
@@ -23,7 +21,7 @@ date
 date
 ./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID11 --to martin.sotheran@continuumbridge.com
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID12 --to "carson.bradbury@continuumbridge.com, martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+./CBr_mailBW.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID12 --to "carson.bradbury@continuumbridge.com, martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
 ./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID22 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
@@ -31,5 +29,5 @@ date
 date
 ./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID53 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID66 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, Praminda.Caleb-solly@uwe.ac.uk, Amalia.Tsanaka@uwe.ac.uk"
+./CBr_mailBW.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID66 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, Praminda.Caleb-solly@uwe.ac.uk, Amalia.Tsanaka@uwe.ac.uk"
 
--- geras_utils/backup_geras.py
+++ geras_utils/backup_geras.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # backup_geras.py
-# Copyright (C) ContinuumBridge Limited, 2014 - All Rights Reserved
+# Copyright (C) ContinuumBridge Limited, 2015 - All Rights Reserved
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
 # Written by Martin Sotheran
@@ -25,17 +25,35 @@ def nicedate(timeStamp):
 @click.command()
 @click.option('--key', prompt='Geras master (write) API key', help='Your Geras API key. See http://geras.1248.io/user/apidoc.')
 @click.option('--dir', prompt='Where to write the backup files', help='Absolute path to where you want the backups.')
-def backup_geras(key, dir):
+@click.option('--bid', nargs=1, help='The bridge ID to backup.')
+
+def backup_geras(key, dir, bid):
 
     print "Requesting list of series"    
-    r = requests.get('http://geras.1248.io/serieslist', auth=(key,''))
-    allseries = json.loads(r.content)    
+    try:
+        r = requests.get('http://geras.1248.io/serieslist', auth=(key,''))
+        allseries = json.loads(r.content)    
+    except:
+        print "** Series list request failed"
+        exit() 
+
+    allBridges = 0
+    if not bid:
+        print "No BID specified - doing them all"
+        allBridges = 1
     
     bidlist = []    
-    for s in allseries:
-        ss = s.split('/')
-        if not ss[1] in bidlist:
-           bidlist.append(ss[1])                
+    if allBridges:
+        for s in allseries:
+            ss = s.split('/')
+            if not ss[1] in bidlist:
+                bidlist.append(ss[1])                
+    else:
+        for s in allseries:
+            ss = s.split('/')
+            if not ss[1] in bidlist and (bid+"/") in s:
+                bidlist.append(ss[1])                
+    
     print "Backing up:"
     print (json.dumps(bidlist, indent=4))            
     
@@ -43,15 +61,32 @@ def backup_geras(key, dir):
                     
     for s in bidlist:
         url = 'http://geras.1248.io/series?pattern=%2F'+ s +'%2F%23'
-        print "Getting:", url
-        r = requests.get(url, auth=('ea2f0e06ff8123b7f46f77a3a451731a',''))
-        alldata = json.loads(r.content)    
-        #print (json.dumps(alldata, indent=4))
-        f = dir + "/" + now + s + ".txt"
-        with open(f, 'w') as outfile:
-            json.dump(alldata, outfile)
-        print "Written:", f, "\n"
-        time.sleep(1) # probably worth being a bit gentle with geras
+        retry = 0
+        max_retries = 10
+        waitTime = 240
+        while retry < max_retries:
+            retry += 1
+            try:
+                print "Getting:", url, "try:", retry
+                r = requests.get(url, auth=('ea2f0e06ff8123b7f46f77a3a451731a',''))
+                alldata = json.loads(r.content)
+                f = dir + "/" + now + s + ".txt"
+                with open(f, 'w') as outfile:
+                    json.dump(alldata, outfile)
+                print "Written:", len(r.text), "items to", f, "\n"
+                if allBridges:
+                    time.sleep(waitTime) # I'm sure they're throttling heavy users
+                break    
+            except:
+                print "**",s,"Failed: Status code:", r.status_code, r.reason, "Got approx", len(r.text), "items"
+                r.raise_for_status()
+                print "r.raise_for_status():", r.raise_for_status()
+                if retry == max_retries:
+                    print s, "failed completely"
+                    exit()
+                print "Waiting", retry*waitTime/60, "minutes"
+                time.sleep(retry*waitTime) # I'm sure they're throttling heavy users             
+            
                                
 if __name__ == '__main__':
     backup_geras()
