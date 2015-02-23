88cb80bd046eff763260ca8a1db4ace7aaa0758a
PeterClaydon <peter.claydon@continuumbridge.com>
2015-02-23 11:19:45 +0000

    Changed password

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -6,28 +6,28 @@
 cd /home/bridge/bridge_admin/emailing
 # Sirona
 date
-./sch_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID36 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com, martyn.price@sirona-cic.org.uk"
+./sch_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID36 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com, martyn.price@sirona-cic.org.uk"
 # CBr
 cd /home/bridge/bridge_admin/emailing/CBr_mailing
 date
-./CBr_mailBW.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID45 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
+./CBr_mailBW.py --user bridges@continuumbridge.com --password Mucht00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID45 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID59 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
+./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID59 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
 #, richard.tarring@sirona-cic.org.uk" just whilst his bridge is dead
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID6 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID6 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID7 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID7 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID11 --to martin.sotheran@continuumbridge.com
+./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID11 --to martin.sotheran@continuumbridge.com
 date
-./CBr_mailBW.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID12 --to "carson.bradbury@continuumbridge.com, martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+./CBr_mailBW.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID12 --to "carson.bradbury@continuumbridge.com, martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID22 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID22 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID49 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID49 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
-./CBr_mail.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID53 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID53 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
-./CBr_mailBW.py --user bridges@continuumbridge.com --password cbridgest00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID66 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, Praminda.Caleb-solly@uwe.ac.uk, Amalia.Tsanaka@uwe.ac.uk"
+./CBr_mailBW.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID66 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, Praminda.Caleb-solly@uwe.ac.uk, Amalia.Tsanaka@uwe.ac.uk"
 
