bf569b32343d2e93fa7a2612450f071d768ea2ff
PeterClaydon <peter.claydon@continuumbridge.com>
2015-03-17 17:24:25 +0000

    Updating tests

--- testing/server_test.sh
+++ testing/server_test.sh
@@ -17,15 +17,15 @@ echo "Deleting bridge"
 cd ~/apps_dev/adaptor_test_app
 git pull
 echo "Posting app"
-~/bridge_admin/testing/cb --app post config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app post config_staging.json --user $USER --password $PASSWORD
 echo "Getting app"
-~/bridge_admin/testing/cb --app get config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app get config_staging.json --user $USER --password $PASSWORD
 echo "Patching app"
-~/bridge_admin/testing/cb --app patch config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app patch config_staging.json --user $USER --password $PASSWORD
 echo "Getting app"
-~/bridge_admin/testing/cb --app get config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app get config_staging.json --user $USER --password $PASSWORD
 echo "Deleting app"
-~/bridge_admin/testing/cb --app delete config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app delete config_staging.json --user $USER --password $PASSWORD
 #
 cd ~/adaptors_dev/test_adaptor
 git pull
@@ -59,26 +59,26 @@ echo "Deleting bridge"
 cd ~/apps_dev/adaptor_test_app
 git pull
 echo "Posting app"
-~/bridge_admin/testing/cb --app post config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app post config_staging.json --user $USER --password $PASSWORD
 echo "Getting app"
-~/bridge_admin/testing/cb --app get config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app get config_staging.json --user $USER --password $PASSWORD
 echo "Patching app"
-~/bridge_admin/testing/cb --app patch config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app patch config_staging.json --user $USER --password $PASSWORD
 echo "Getting app"
-~/bridge_admin/testing/cb --app get config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app get config_staging.json --user $USER --password $PASSWORD
 echo "Deleting app"
-~/bridge_admin/testing/cb --app delete config.json --user $USER --password $PASSWORD
+~/bridge_admin/testing/cb --app delete config_staging.json --user $USER --password $PASSWORD
 #
-cd ~/adaptors_dev/test_adaptor
-git pull
-echo "Posting device"
-~/bridge_admin/testing/cb --device post config.json --user $USER --password $PASSWORD
-echo "Getting device"
-~/bridge_admin/testing/cb --device get config.json --user $USER --password $PASSWORD
-echo "Patching device"
-~/bridge_admin/testing/cb --device patch config.json --user $USER --password $PASSWORD
-echo "Getting device"
-~/bridge_admin/testing/cb --device get config.json --user $USER --password $PASSWORD
-echo "Deleting device"
-~/bridge_admin/testing/cb --device delete config.json --user $USER --password $PASSWORD
+#cd ~/adaptors_dev/test_adaptor
+#git pull
+#echo "Posting device"
+#~/bridge_admin/testing/cb --device post config.json --user $USER --password $PASSWORD
+#echo "Getting device"
+#~/bridge_admin/testing/cb --device get config.json --user $USER --password $PASSWORD
+#echo "Patching device"
+#~/bridge_admin/testing/cb --device patch config.json --user $USER --password $PASSWORD
+#echo "Getting device"
+#~/bridge_admin/testing/cb --device get config.json --user $USER --password $PASSWORD
+#echo "Deleting device"
+#~/bridge_admin/testing/cb --device delete config.json --user $USER --password $PASSWORD
 #
