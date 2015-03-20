fe03c6ff658320087c1ed6f0c6895b171b427df5
Martin Sotheran <martin.sotheran@continuumbridge.com>
2015-03-20 10:27:55 +0000

    moved cbr-59 to sirona list. Not going to RT just yet

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -9,11 +9,11 @@ date
 ./sch_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID36 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com, martyn.price@sirona-cic.org.uk"
 date
 ./sch_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID45 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com, Mandy.Miles@sirona-cic.org.uk"
+date
+./sch_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID59 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
+#, richard.tarring@sirona-cic.org.uk" just whilst we make sure it's ok
 # CBr
 cd /home/bridge/bridge_admin/emailing/CBr_mailing
-date
-./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key b9b0c3fea6308127164de1616cb97723 --bid BID59 --to "peter.claydon@continuumbridge.com, martin.sotheran@continuumbridge.com"
-#, richard.tarring@sirona-cic.org.uk" just whilst his bridge is dead
 date
 ./CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID11 --to martin.sotheran@continuumbridge.com
 date
