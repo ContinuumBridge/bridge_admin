b6c85d9aefd9b1ed6cfc67138daf14e66fa42753
martinxs <martin.sotheran@continuumbridge.com>
2015-03-26 22:01:00 +0000

    experiment with influx mailer

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -20,4 +20,9 @@ date
 ./CBr_mailBW.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID12 --to "carson.bradbury@continuumbridge.com, martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 date
 ./CBr_mailBW.py --user bridges@continuumbridge.com --password Mucht00f@r --key c685297d8c0f710e3bd1c8e771eb8d3d --bid BID66 --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, Praminda.Caleb-solly@uwe.ac.uk, Amalia.Tsanaka@uwe.ac.uk"
-
+# As an experiment
+cd /home/ubuntu/bridge_admin/ifx_utils/CBr_mailing
+date
+./ifx_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID12 --db "Bridges" --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --template "CBr_table_template.htm"
+date
+./ifx_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID36 --db "Bridges" --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --template "CBr_table_template.htm"
