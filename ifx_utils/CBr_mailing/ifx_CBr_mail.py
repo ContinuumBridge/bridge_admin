d871966680489c3afcbacd954442d86b6cccee56
martinxs <martin.sotheran@continuumbridge.com>
2015-03-26 22:00:30 +0000

    experiment with influx mailer

---
+++ ifx_utils/CBr_mailing/ifx_CBr_mail.py
@@ -0,0 +1,374 @@
+#!/usr/bin/env python
+# CBr_mail_ifx.py
+# Copyright (C) ContinuumBridge Limited, 2014 - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Martin Sotheran
+#
+
+gerasurl = 'http://geras.1248.io/'
+import requests
+import json
+import time
+import click
+import os, sys
+import re
+import smtplib
+from itertools import cycle
+import urllib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.MIMEImage import MIMEImage
+
+#Constants
+tenMinutes         = 10 * 60
+oneHour            = 60 * 60
+oneDay             = oneHour * 24
+dburl = "http://onepointtwentyone-horsebrokedown-1.c.influxdb.com:8086/"
+
+def nicetime(timeStamp):
+    localtime = time.localtime(timeStamp)
+    now = time.strftime('%Y-%m-%d %H:%M:%S', localtime)
+    return now
+
+def nicedate(timeStamp):
+    localtime = time.localtime(timeStamp)
+    now = time.strftime('%Y-%m-%d', localtime)
+    return now
+
+def nicehours(timeStamp):
+    localtime = time.localtime(timeStamp)
+    now = time.strftime('%H:%M:%S', localtime)
+    return now
+
+def stepHourMin(timeStamp):
+    localtime = time.localtime(timeStamp)
+    now = time.strftime('%H%M', localtime)
+    return now
+
+def epochtime(date_time):
+    pattern = '%Y-%m-%d %H:%M:%S'
+    epoch = int(time.mktime(time.strptime(date_time, pattern)))
+    return epoch
+
+def start():
+    t = time.localtime(time.time() - oneDay)
+    yesterday = time.strftime('%Y-%m-%d', t)
+    s = yesterday + " 12:00:00"
+    return epochtime(s)
+
+def activeInTenMinutes(series, time):
+    for s in series:
+        if s["t"] >= time and s["t"] < time + tenMinutes:
+            return True
+    return False
+
+def powerInTenMinutes(series, time):
+    for s in series:
+        if s["t"] >= time and s["t"] < time + tenMinutes:
+            if s["v"] > 3.5:
+                return True
+    return False
+    
+def tempInTenMinutes(series, time):
+    for s in series:
+        if s["t"] >= time and s["t"] < time + tenMinutes:
+            return "%2.1f" %s["v"]
+    return ""
+
+def replace(holder, value):
+    global h1, h2, working
+    if working == "h1":
+        h2 = h1.replace(holder, value)
+        working = "h2"
+    else:
+        h1 = h2.replace(holder, value)
+        working = "h1"
+
+@click.command()
+@click.option('--user', nargs=1, help='User name of email account.')
+@click.option('--password', prompt='Password', help='Password of email account.')
+@click.option('--bid', nargs=1, help='The bridge ID to list.')
+@click.option('--db', nargs=1, help='The database to look in')
+@click.option('--to', nargs=1, help='The address to send the email to.')
+@click.option('--template', nargs=1, help='the table template file.')
+
+def cbr_email_ifx(user, password, bid, to, db, template):
+    h1 = ""
+    h2 = ""
+    working = ""
+    startTime = start()
+    endTime = startTime + oneDay
+    serieslist = []
+    """ 
+    Expected timeseries: 
+    {
+    "/BID11/MagSW_ES-Kitchen-Cup_Cupboard/binary": {
+        "e": [
+            {
+                "v": 0, 
+                "t": 1427148242, 
+                "n": "/BID11/MagSW_ES-Kitchen-Cup_Cupboard/binary"
+            }, 
+            {
+                "v": 1, 
+                "t": 1427148243, 
+                "n": "/BID11/MagSW_ES-Kitchen-Cup_Cupboard/binary"
+            }, 
+    We've got:-
+    [
+    {
+        "points": [
+            [
+                1427193906669, 
+                15927010001, 
+                50
+            ], 
+            [
+                1427186706876, 
+                15910230001, 
+                50
+            ], 
+
+    """
+    timeseries = {}
+    if not bid:
+        print "You must provide a bridge ID using the --bid option."
+        exit()
+    else:
+        #select * from /BID11/ where time > 1427025600s and time < 1427112000s
+        q = "select * from /" + bid + "/ where time >" + str(startTime) + "s and time <" + str(endTime) + "s"
+        query = urllib.urlencode ({'q':q})
+
+        print "Requesting list of series from", nicetime(startTime), "to", nicetime(endTime)
+        url = dburl + "db/" + db + "/series?u=root&p=27ff25609da60f2d&" + query 
+        print "fetching from:", url
+        r = requests.get(url)
+        pts = r.json()
+        #print json.dumps(r.json(), indent=4)
+        #print json.dumps(r.content, indent=4)
+        for i in range(0, len(pts)):
+            serieslist.append("/" + pts[i]["name"])
+            sensor = "/"+pts[i]["name"]
+            vt = []            
+            #print "Processing:", json.dumps(serieslist, indent=4)
+            #print "Got:", len(pts[i]["points"]), "points for", pts[i]["name"]
+            for j in range(0, len(pts[i]["points"])):
+                t = pts[i]["points"][j][0]/1000
+                v = pts[i]["points"][j][2]
+                #print "sensor,t,v:", sensor, t, v
+                vt.append({"v":v, "t":t})
+            timeseries[sensor] = {"e":vt}
+                
+           
+    # Read HTML file
+    with open(template, "r") as f:
+        h1 = f.read()
+    # Because there can be "illigal" ASCII characters in the HTML file:
+    i = 0
+    for c in h1:
+        if ord(c) > 127:
+            #print "Replaced:", c, ord(c)
+            h2 += " "
+        else:
+            h2 += c
+    # Headers
+    h1 = h2.replace("nnn", bid)
+    h2 = h1.replace("&lt;date1&gt;", nicedate(startTime))
+    h1 = h2.replace("&lt;date2&gt;", nicedate(endTime)+" (InfluxDB)")
+    working = "h1"
+
+    #print "timeseries:", json.dumps(timeseries, indent=4)
+    col = 1
+    prev = "fubar"
+    for path in serieslist:
+        if not("Mesh" in path or "battery" in path or "connected" in path or "luminance" in path or "magnet" in path or "button" in path or "ir_temperature" in path or ("tag_ti" in path.lower() and "temperature" in path) or ("tbk" in path.lower() and "binary" in path.lower())):
+        
+#        ("binary" in path.lower() and "coffee" in path.lower()) or        
+#        ("binary" in path.lower() and "coffee_cupboard" in path.lower())):                
+                    
+            series = timeseries[path]["e"]
+            
+            # split it into BID, Name, Type (_ is a sledgehammer - see below)
+            #ss = re.split('\W+|/|-|_',path)
+            ss = re.split('\W+|/|-',path)            
+            print "First ss:",ss
+
+            # Change some "types" according to sensor type
+            length = len(ss)
+            for i in range(0,len(ss)):
+                if "pir" in ss[i].lower():
+                    ss[length-1] = ss[length-1].replace("binary", "Activity")
+                            
+            if "binary" in ss:
+                if "Kettle" or "Door" in ss: 
+                    del ss[ss.index("binary")]            
+            #print "1. What's left?:",ss            
+            
+            # get rid of blank field, BID & sensor type unless it's a KM
+            if "KM" in ss:
+                del ss[0:2]
+            elif "Night_Wander" in ss:
+                del ss[3]
+                del ss[0:2]
+            else:
+                del ss[0:3]
+            #print "   2. What's left now?:",ss                           
+            
+            # squash it down to three lines for the template
+            if len(ss) > 4:
+                ss[0] = ss[0] + " " + ss[1]
+                del ss[1]
+                ss[1] = ss[1] + " " + ss[2]
+                del ss[2]
+            elif len(ss) > 3:                
+                ss[1] = ss[1] + " " + ss[2]
+                del ss[2]
+                        
+            # There are four possible fields plus the type. 
+            # And there should only be underscores left
+            for i in range(0,len(ss)):
+                ss[i] = ss[i].replace("_", " ")
+                #print "ss[",i,"]:", ss[i], "\n"            
+            #print "      3. What's left now?:",ss            
+
+            for value in ss[0:len(ss)]:
+                holder = "S_" + str(col) + "_name" + str(ss.index(value)+1)
+                print "holder:", holder, " value:", value
+                if working == "h1":
+                    h2 = h1.replace(holder, value)
+                    working = "h2"
+                else:
+                    h1 = h2.replace(holder, value)
+                    working = "h1"
+            
+
+            # build table entries
+            if series and "magsw" in path.lower():  # Always on doors & drawers for now
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    if activeInTenMinutes(series, stepTime):
+                        value = "Open"
+                    else:
+                        value = ""
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
+            elif series and "power" in path.lower():
+                prev_power = "none"
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    value = powerInTenMinutes(series, stepTime)                  
+                    if value == "":
+                        if prev_power != "none":
+                            value = prev_power
+                    else:
+                        prev_power = value
+                        
+                    if value:
+                        op = "On"
+                    else:
+                        op = ""
+
+                    if working == "h1":
+                        h2 = h1.replace(holder, op)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, op)
+                        working = "h1"
+            elif series and "temperature" in path.lower():
+                prev_temperature = "none"
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    value = tempInTenMinutes(series, stepTime)
+                    if value == "":
+                        if prev_temperature != "none":
+                            value = prev_temperature
+                    else:
+                        prev_temperature = value
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
+            elif series and "pir" in path.lower() and "binary" in path.lower():
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    if activeInTenMinutes(series, stepTime):
+                        value = "A"
+                    else:
+                        value = ""
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
+            else:
+                for stepTime in range(startTime, startTime + oneDay, tenMinutes):
+                    holder = "S_" + str(col) + "_" + stepHourMin(stepTime)
+                    value = "-"
+                    if working == "h1":
+                        h2 = h1.replace(holder, value)
+                        working = "h2"
+                    else:
+                        h1 = h2.replace(holder, value)
+                        working = "h1"
+            print "\n"
+            col += 1
+            
+
+    # Remove any unused holders
+    if working == "h1":
+        h2 = re.sub("S_[0-9]+_name[0-9]+", "", h1, 0)
+        h1 = re.sub("S_[0-9]+_[0-9]+", "", h2, 0)
+    else:
+        h1 = re.sub("S_[0-9]+_name[0-9]+", "", h2, 0)
+        h2 = re.sub("S_[0-9]+_[0-9]+", "", h1, 0)
+    if working == "h1":
+        htmlText = h1
+    else:
+        htmlText = h2
+
+
+    #exit()
+    
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Activity for bridge " + bid + " from " + nicedate(startTime) + " to " + nicedate(endTime) + " from InfluxDB"
+    msg['From'] = "Bridges <bridges@continuumbridge.com>"
+    recipients = to.split(',')
+    [p.strip(' ') for p in recipients]
+    if len(recipients) == 1:
+        msg['To'] = to
+    else:
+        msg['To'] = ", ".join(recipients)
+    # Create the body of the message (a plain-text and an HTML version).
+    text = "Content only available with HTML email clients\n"
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(htmlText, 'html')
+    
+    fp = open('image001.png', 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+    # Define the image's ID as referenced above
+    msgImage.add_header('Content-ID', '<image001.png>')
+    msg.attach(msgImage)
+    msg.attach(part1)
+    msg.attach(part2)
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(user, password)
+    mail.sendmail(user, recipients, msg.as_string())
+    mail.quit()
+
+                  
+if __name__ == '__main__':
+    cbr_email_ifx()
+
