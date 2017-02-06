fe1d1c152cf92d8ea92c81152b2fb727c0b712eb
martinxs <martin.sotheran@continuumbridge.com>
2017-02-06 12:58:08 +0000

    simple .sh

---
+++ ifx_utils/DYH/dyh.sh
@@ -0,0 +1,4 @@
+cd /home/martin/bridge_admin/ifx_utils/DYH
+date
+./dyh.py --bid 264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, martin.sotheran@gmail.com" --daysago 0
+date
