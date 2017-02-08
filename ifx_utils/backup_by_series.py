16eb99b79cd46f5d70aa42f99876327ad5f92c79
martinxs <martin.sotheran@continuumbridge.com>
2017-02-08 17:44:52 +0000

    now fetches a series at a time rather than a bridge

--- ifx_utils/backup_by_series.py
+++ ifx_utils/backup_by_series.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# backup_geras_by_series.py
+# backup_by_series.py
 # Copyright (C) ContinuumBridge Limited, 2015 - All Rights Reserved
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
@@ -27,8 +27,7 @@ dburl = "http://onepointtwentyone-horsebrokedown-1.c.influxdb.com:8086/"
 @click.option('--folder', nargs=1, help='The absolute path')
 def backup_by_series(db, folder):
 
-    allseries=[]
-    bidList = []
+    allSeries=[]
 
     q = "select * from /.*/ limit 1"
     query = urllib.urlencode ({'q':q})
@@ -39,42 +38,32 @@ def backup_by_series(db, folder):
     r = requests.get(url)
     pts = r.json()     
     for s in pts:
-        ss = re.split('\W+|/|-',s["name"])                    
-        if "BID" in ss[0]:
-            if ss[0] not in bidList:
-                bidList.append(ss[0])
-        else:
-            print "rejected:", ss
+        if "BID" in s["name"]:
+            allSeries.append(s["name"])
 
-    print "going to do", json.dumps(bidList, indent=4)
-
-
-    for bid in bidList:
-        allseries = []
-        q = "select * from /" + bid + ".*/" # limit 5"
+    print "going to do series", json.dumps(allSeries, indent=4)
+    for s in allSeries:
+        q = 'select * from "' + s + '"' # limit 5'
         query = urllib.urlencode ({'q':q})
-        print "Requesting data for", bid
+        #print "Requesting data for", query
         url = dburl + "db/" + db + "/series?u=root&p=27ff25609da60f2d&" + query 
-        print "fetching from:", url
-        r = requests.get(url)
-        pts = r.json()     
-        for s in pts:
-            allseries.append(s["name"])
-        
-  
-        print "Backing up:"
-        print (json.dumps(allseries, indent=4))            
-    
+        #print "fetching from:", url
+        try:
+            r = requests.get(url)
+            pts = r.json()     
+        except:
+            print "Fetch failed"
+            exit()        
+
         now = time.strftime('%Y-%m-%d_', time.localtime())
-    
-        for s in pts:
-            f = folder + "/" + now + db + "/" + s["name"] + ".txt"
-            #print "writing:", json.dumps(s, indent=4), "to", f
+        for m in pts:
+            f = folder + "/" + now + db + "/" + m["name"] + ".txt"
+            #print "writing:", json.dumps(m, indent=4), "to", f
             if not os.path.exists(os.path.dirname(f)):
                 os.makedirs(os.path.dirname(f))            
             with open(f, 'w') as outfile:
-                json.dump(s, outfile)
-            print "    Written:", len(s["points"]), "items to", f
+                json.dump(m, outfile)
+            print "Written:", len(m["points"]), "items to", f
 
 if __name__ == '__main__':
     backup_by_series()
