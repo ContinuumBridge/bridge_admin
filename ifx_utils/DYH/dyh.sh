381697fa90dac3329f0cdcdecdd76559ece5a4ab
martinxs <martin.sotheran@continuumbridge.com>
2017-03-03 14:30:05 +0000

    used BID264 in .sh to generate correct activity file and generated up to date activity file

--- ifx_utils/DYH/dyh.sh
+++ ifx_utils/DYH/dyh.sh
@@ -1,6 +1,6 @@
 cd /home/ubuntu/bridge_admin/ifx_utils/DYH 
 date
-./dyh.py --bid 264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, steve.barraclough@continuumbridge.com, peter.claydon@continuumbridge.com" --doors "" --daysago 0
+./dyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, steve.barraclough@continuumbridge.com, peter.claydon@continuumbridge.com" --doors "" --daysago 0
 date
-./dyh.py --bid 264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "Phillipa.Parnell@thedtgroup.org, martin.sotheran@continuumbridge.com" --doors "" --daysago 0
+./dyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "Phillipa.Parnell@thedtgroup.org, martin.sotheran@continuumbridge.com" --doors "" --daysago 0
 date
