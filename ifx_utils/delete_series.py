c3d1978535c60426c6f54dd43b41731f062e0c1c
martinxs <martin.sotheran@continuumbridge.com>
2017-04-05 17:33:10 +0100

    updated and added a search string

--- ifx_utils/delete_series.py
+++ ifx_utils/delete_series.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-# latest_data_ifx.py
-# Copyright (C) ContinuumBridge Limited, 2015 - All Rights Reserved
+# delete_series.py
+# Copyright (C) ContinuumBridge Limited, 2017 - All Rights Reserved
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
 # Written by Martin Sotheran
 #
-# Usage  ./delete_series.py --bid "BID101" --db "Bridges" --time_limit 120
-# requires the latest latest_data_ifx.py
+# Usage  ./delete_series.py --bid "BID101" --db "Bridges" --select "string"
+# Find and delete a series in influxDB
 
 import requests
 import json
@@ -15,58 +15,75 @@ import click
 import os, sys
 import urllib #re
 from itertools import cycle
-from latest_data_ifx import latest_data
 
 dburl = "http://onepointtwentyone-horsebrokedown-1.c.influxdb.com:8086/"
 @click.command()
 @click.option('--bid', nargs=1, help='The bridge ID to check.')
 @click.option('--db', nargs=1, help='The name of the influx database.')
-@click.option('--time_limit', nargs=1, help='how many days since last write to series')
+@click.option('--select', nargs=1, help='string to filter what to find')
 
-def delete_series (bid, db, time_limit):
-    go_for_it = 0
-    n = 0
-    ip = "nonesense"
- 
-    sensorDates = latest_data(bid, db) 
-    print "\nThe following are >", time_limit, "days old"
-    if not sensorDates:
-        print "No data, bombing out"
+def delete_series (bid, db, select):
+    if not bid:
+        print "No BID specified - can't continue"
         exit()
-    for p in sensorDates:
-        if p["days_old"] >= int(time_limit):
-            n += 1
-            print  "    ", p["name"], p["days_old"], "days old"
-    if n == 0:
-        print "    Nothing older than", time_limit, "days"
     else:
-        ip = raw_input("\nDelete everything older than time_limit, confirm each one or exit? (Everything/c/e):")
+        q = "select * from /" + bid + "/ limit 1"
+        query = urllib.urlencode ({'q':q})
+
+    url = dburl + "db/" + db + "/series?u=root&p=27ff25609da60f2d&" + query 
+    print "fetching from:", url
+    try:
+        r = requests.get(url) # ,params=list+series)
+        latestPoints = r.json()
+    except:
+        print "****No data found in the", db, "database. Probably", bid, "isn't in there****"
+        exit()
+
+    ip = "nonesense"
+
+    target = [] 
+    for p in latestPoints:
+	if select in p["name"]:
+	    target.append(p)
+
+    if target:
+	print "targets are:"
+        for xy in target:
+	    print xy["name"]
+    else:
+	print "no matches found"
+	exit()
+
+    ip = raw_input("\nDelete ALL these, confirm each one or exit? (ALL/c/e):")
     if ip == "e":
         exit()
-
-    for p in sensorDates:
-        if p["days_old"] >= int(time_limit):
-            if ip == "Everything":
-                print "Dropping", p["name"]
-                q = 'DROP SERIES "' + p["name"] + '"'
-                query = urllib.urlencode ({'q':q})    
-                url = dburl + "db/" + db + "/series?u=root&p=27ff25609da60f2d&" + query 
-                try:
-                    r = requests.get(url)
-                except:
-                    print "couldn't drop", p["name"]
-            else:
-                print "delete:", p["name"], p["days_old"], "days old?"
-                ip = raw_input("Y/n:") 
-                if ip == "Y":
-                    q = 'DROP SERIES "' + p["name"] + '"'
-                    query = urllib.urlencode ({'q':q})    
-                    url = dburl + "db/" + db + "/series?u=root&p=27ff25609da60f2d&" + query 
-                    try:
-                        r = requests.get(url)
-                    except:
-                        print "couldn't drop", p["name"]
-
+    elif ip == "ALL":
+        for tg in target:
+	    q = 'DROP SERIES "' + tg["name"] + '"'
+	    query = urllib.urlencode ({'q':q})    
+	    url = dburl + "db/" + db + "/series?u=root&p=27ff25609da60f2d&" + query 
+	    try:
+		r = requests.get(url)
+		print "Dropped", tg["name"]
+	    except:
+		print "couldn't drop", tg["name"]
+    elif ip =="c":
+        for tg in target:
+	    print "delete:", tg["name"], "?"
+	    ip = raw_input("Yes/no/exit(Y/n/e):") 
+	    if ip == "e":
+		exit()
+	    if ip == "Y":
+		q = 'DROP SERIES "' + tg["name"] + '"'
+		query = urllib.urlencode ({'q':q})    
+		url = dburl + "db/" + db + "/series?u=root&p=27ff25609da60f2d&" + query 
+		try:
+		    r = requests.get(url)
+		    print "Dropped", tg["name"]
+		except:
+		    print "couldn't drop", tg["name"]
+    else:
+	exit()
 
 if __name__ == '__main__':
     delete_series()
