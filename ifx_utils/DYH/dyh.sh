d05022a63587d0a47a61559676ba4a605e6d7e94
martinxs <martin.sotheran@continuumbridge.com>
2017-04-25 18:06:59 +0100

    First pass using the visitor button. Disabled the shower mails

--- ifx_utils/DYH/dyh.sh
+++ ifx_utils/DYH/dyh.sh
@@ -4,9 +4,9 @@ date
 date
 ./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "Jenny.Orr@thedtgroup.org, martin.sotheran@continuumbridge.com" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
 date
-./ndyh.py --bid BID267 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "" --shower_mail "y" --doors "" --writetoifx "" --daysago 0
-date
-./ndyh.py --bid BID11 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "" --shower_mail "y" --doors "" --writetoifx "" --daysago 0
-date
+#./ndyh.py --bid BID267 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "" --shower_mail "y" --doors "" --writetoifx "" --daysago 0
+#date
+#./ndyh.py --bid BID11 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "" --shower_mail "y" --doors "" --writetoifx "" --daysago 0
+#date
 #./dyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "Phillipa.Parnell@thedtgroup.org, martin.sotheran@continuumbridge.com" --doors "" --mail "y" --daysago 0
 #date
