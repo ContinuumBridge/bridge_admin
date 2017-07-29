7a7ebb54d05d6d7032a18ecad155ce0b57d727c0
PeterClaydon <peter.claydon@continuumbridge.com>
2017-07-29 18:31:17 +0100

    Committing current state for new server

--- emailing/sch_list.sh
+++ emailing/sch_list.sh
@@ -10,26 +10,23 @@ cd /home/ubuntu/bridge_admin/ifx_utils/CBr_mailing
 
 # CBr
 date
-./ifx_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID66 --db "Bridges" --template "CBr_table_template.htm" --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, Praminda.Caleb-solly@uwe.ac.uk"
+./ifx_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID66 --db "Bridges" --template "2016-12-05_CBr_table_template.htm" --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, Praminda.Caleb-solly@uwe.ac.uk"
 
 date
-./friendly_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID241 --db "Bridges" --template "CBr_table_template.htm" --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, steve.barraclough@continuumbridge.com"
+./friendly_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID241 --db "Bridges" --template "2016-12-05_CBr_table_template.htm" --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, steve.barraclough@continuumbridge.com"
 
-#date
-#./ifx_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID12 --db "Bridges" --template "CBr_table_template.htm" --to "martin.sotheran@continuumbridge.com, carson.bradbury@continuumbridge.com, peter.claydon@continuumbridge.com"
-
-#date
-#./ifx_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID11 --db "Bridges" --template "CBr_table_template.htm" --to "martin.sotheran@continuumbridge.com"
+date
+./friendly_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID11 --db "Bridges" --template "2016-12-05_CBr_table_template.htm" --to "martin.sotheran@continuumbridge.com"
 
 # Sirona
-date
-./friendly_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID36 --db "SCH" --template Sirona_table_template.htm --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com, martyn.price@sirona-cic.org.uk"
-date
-./friendly_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID45 --db "SCH" --template Sirona_table_template.htm --to "simon.allen@sirona-cic.org.uk, martin.sotheran@continuumbridge.com"
+#date
+#./friendly_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID36 --db "SCH" --template Sirona_table_template.htm --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
+#date
+#./friendly_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID45 --db "SCH" --template Sirona_table_template.htm --to "simon.allen@sirona-cic.org.uk, martin.sotheran@continuumbridge.com"
 #date
 #./friendly_CBr_mail.py --user bridges@continuumbridge.com --password Mucht00f@r --bid BID67 --db "SCH" --template Sirona_table_template.htm --to "christopher.burfield@sirona-cic.org.uk, martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com"
 
 # Generate Sirona data warehouse CSV file
-cd /home/ubuntu/bridge_admin/ifx_utils
-date
-./warehouse_ifx.py --db "SCH" --bids "BID36" "BID67" "BID45" --daysago 1
+#cd /home/ubuntu/bridge_admin/ifx_utils
+#date
+#./warehouse_ifx.py --db "SCH" --bids "BID36" "BID67" "BID45" --daysago 1
