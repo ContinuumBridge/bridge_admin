14a8962a9bf1c02090585ffb8c5b1e754c3a4cf2
martinxs <martin.sotheran@continuumbridge.com>
2017-08-07 11:01:12 +0100

    This time with Jenny's address

--- ifx_utils/DYH/dyh.sh
+++ ifx_utils/DYH/dyh.sh
@@ -2,4 +2,4 @@ cd /home/ubuntu/bridge_admin/ifx_utils/DYH
 date
 ./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "y" --shower_mail "" --doors "" --writetoifx "" --daysago 0
 date
-./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "jessica.hewitson@thedtgroup.org, martin.sotheran@continuumbridge.com" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
+./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "Jenny.Orr@thedtgroup.org, martin.sotheran@continuumbridge.com" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
