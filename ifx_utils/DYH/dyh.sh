40c197b710eb826714b26cbf91502d83913764ef
martinxs <martin.sotheran@continuumbridge.com>
2017-08-07 10:58:35 +0100

    New resident - started mails to Jenny

--- ifx_utils/DYH/dyh.sh
+++ ifx_utils/DYH/dyh.sh
@@ -1,8 +1,5 @@
 cd /home/ubuntu/bridge_admin/ifx_utils/DYH 
 date
 ./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "y" --shower_mail "" --doors "" --writetoifx "" --daysago 0
-#date
-#./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "jessica.hewitson@thedtgroup.org, martin.sotheran@continuumbridge.com" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
-#date
-#./ndyh.py --bid BID11 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "" --shower_mail "y" --doors "" --writetoifx "" --daysago 0
-#date
+date
+./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "jessica.hewitson@thedtgroup.org, martin.sotheran@continuumbridge.com" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
