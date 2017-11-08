c0b09a78cf1fdd08a35bff659fa21a55559eff5e
martinxs <martin.sotheran@continuumbridge.com>
2017-11-08 19:03:15 +0000

    Replaced batteries so put uptime threshold back to 26min and Phillipa back on mails

--- ifx_utils/DYH/dyh.sh
+++ ifx_utils/DYH/dyh.sh
@@ -3,4 +3,4 @@ date
 #./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "y" --shower_mail "" --doors "" --writetoifx "" --daysago 0
 #date
 #./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, jessica.hewitson@thedtgroup.org" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
-./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
+./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "Phillipa.Parnell@thedtgroup.org, martin.sotheran@continuumbridge.com" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
