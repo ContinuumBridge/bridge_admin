deed0d7b560be50c86d26ef10d214bb4077be996
martinxs <martin.sotheran@continuumbridge.com>
2019-12-01 14:37:18 +0000

    disabling mails for final switch off

--- ifx_utils/DYH/dyh.sh
+++ ifx_utils/DYH/dyh.sh
@@ -3,7 +3,7 @@ date
 #./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, peter.claydon@continuumbridge.com" --mail "y" --shower_mail "" --doors "" --writetoifx "" --daysago 0
 #date
 #./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com, jessica.hewitson@thedtgroup.org" --mail "y" --doors "" --shower_mail "" --writetoifx "y" --daysago 0
-./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com" --mail "y" --doors "" --writetoifx "" --warning_mails "y" --daysago 0
+#./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@continuumbridge.com" --mail "y" --doors "" --writetoifx "" --warning_mails "y" --daysago 0
 #./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "martin.sotheran@gmail.com" --mail "y" --doors "" --writetoifx "" --warning_mails "" --daysago 0
-./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "Linsey.Norris@thedtgroup.org, Janet.Hodgson@thedtgroup.org" --mail "y" --doors "" --writetoifx "" --warning_mails "" --daysago 0
+#./ndyh.py --bid BID264 --user bridges@continuumbridge.com --db Bridges --password Mucht00f@r --to "Linsey.Norris@thedtgroup.org, Janet.Hodgson@thedtgroup.org" --mail "y" --doors "" --writetoifx "" --warning_mails "" --daysago 0
 
